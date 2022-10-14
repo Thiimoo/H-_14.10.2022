3448f55320646a6d0cb8be743170d308d62092ea
Thiimoo <timo.reitboeck@gmx.at>
2022-10-14 12:02:40 +0200

    tasks init

--- reciprocal_sum_student_stub/src/net/eaustria/ReciprocalArraySum.java
+++ reciprocal_sum_student_stub/src/net/eaustria/ReciprocalArraySum.java
@@ -14,52 +14,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
-
-/**
- * Class wrapping methods for implementing reciprocal array sum in parallel.
- */
 public final class ReciprocalArraySum {
-
-    /**
-     * Default constructor.
-     */
     private ReciprocalArraySum() {
     }
-
-    /**
-     * Sequentially compute the sum of the reciprocal values for a given array.
-     *
-     * @param input Input array
-     * @return The sum of the reciprocals of the array input
-     */
     protected static double seqArraySum(final double[] input) {
         double sum = 0;
-
-        // ToDo: Compute sum of reciprocals of array elements
-       
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i];
+        }
+        return sum;
     }
-  
-
-    /**
-     * This class stub can be filled in to implement the body of each task
-     * created to perform reciprocal array sum in parallel.
-     */
     private static class ReciprocalArraySumTask extends RecursiveAction {
-        /**
-         * Starting index for traversal done by this task.
-         */
         private final int startIndexInclusive;
-        /**
-         * Ending index for traversal done by this task.
-         */
         private final int endIndexExclusive;
-        /**
-         * Input array to reciprocal sum.
-         */
         private final double[] input;
-        /**
-         * Intermediate value produced by this task.
-         */
         private double value;
         
         private static int SEQUENTIAL_THRESHOLD = 50000;
@@ -77,11 +45,6 @@ public final class ReciprocalArraySum {
             this.endIndexExclusive = setEndIndexExclusive;
             this.input = setInput;
         }
-
-        /**
-         * Getter for the value produced by this task.
-         * @return Value produced by this task
-         */
         public double getValue() {
             return value;
         }
@@ -91,7 +54,15 @@ public final class ReciprocalArraySum {
             // TODO: Implement Thread forking on Threshold value. (If size of
             // array smaller than threshold: compute sequentially else, fork 
             // 2 new threads
-           
+           if(input.length < value)
+           {
+
+           }
+           else {
+               ReciprocalArraySumTask task1 = new ReciprocalArraySumTask(0, input.length/2,input);
+               ReciprocalArraySumTask task2 = new ReciprocalArraySumTask((input.length/2)+1, input.length,input);
+                invokeAll(task1,task2);
+           }
             
         }
     }
